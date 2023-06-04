1ca1491ef6560d5b9885a52f3875afa20b3e1352
izelozarslan <izel.ozarslan@gmail.com>
2023-06-04 15:21:47 +0300

    Add city mapper & repository & service

--- src/main/java/com/izelozarslan/weatherapp/repository/CityRepository.java
+++ src/main/java/com/izelozarslan/weatherapp/repository/CityRepository.java
@@ -3,6 +3,11 @@ package com.izelozarslan.weatherapp.repository;
 import com.izelozarslan.weatherapp.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CityRepository extends JpaRepository<City,Long> {
+
+    List<City> findCitiesByUserId(Long userId);
+
 
 }
--- src/main/java/com/izelozarslan/weatherapp/service/CityService.java
+++ src/main/java/com/izelozarslan/weatherapp/service/CityService.java
@@ -5,9 +5,20 @@ import com.izelozarslan.weatherapp.general.BaseEntityService;
 import com.izelozarslan.weatherapp.repository.CityRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CityService extends BaseEntityService<City, CityRepository> {
+
+    private CityRepository repository;
+
     public CityService(CityRepository repository) {
         super(repository);
+        this.repository = repository;
     }
+
+    public List<City> findByUserId(Long userId){
+        return repository.findCitiesByUserId(userId);
+    }
+
 }
