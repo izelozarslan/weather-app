9b0c10dca19b9cfd82bee45c18cf912c90b41e21
izelozarslan <izel.ozarslan@gmail.com>
2023-06-05 14:18:31 +0300

    Add weather error message

---
+++ src/main/java/com/izelozarslan/weatherapp/errormessages/WeatherErrorMessage.java
@@ -0,0 +1,23 @@
+package com.izelozarslan.weatherapp.errormessages;
+
+import com.izelozarslan.weatherapp.general.BaseErrorMessage;
+
+public enum WeatherErrorMessage implements BaseErrorMessage {
+    WEATHER_NOT_FOUND("Weather not found"),
+    WEATHER_DATA_PARSING_ERROR("Weather data parsing error");
+    private final String message;
+
+    WeatherErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
