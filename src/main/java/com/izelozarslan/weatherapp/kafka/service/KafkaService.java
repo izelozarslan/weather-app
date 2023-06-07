86831b18fbee0a2a931d3b680647947581b2ea33
izelozarslan <izel.ozarslan@gmail.com>
2023-06-07 17:50:13 +0300

    Add kafka

---
+++ src/main/java/com/izelozarslan/weatherapp/kafka/service/KafkaService.java
@@ -0,0 +1,26 @@
+package com.izelozarslan.weatherapp.kafka.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public <T> void sendMessage(T record, String topic) {
+        log.info(String.format("%s event => %s", topic, record.toString()));
+        Message<T> message = MessageBuilder
+                .withPayload(record)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build();
+        kafkaTemplate.send(message);
+    }
+}
