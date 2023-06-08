5eaca9f32bf4302354905eff982829f4a58dc26f
izelozarslan <izel.ozarslan@gmail.com>
2023-06-08 03:22:20 +0300

    Update

--- src/main/java/com/izelozarslan/weatherapp/security/auth/dto/RegisterRequestDTO.java
+++ src/main/java/com/izelozarslan/weatherapp/security/auth/dto/RegisterRequestDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterRequest {
+public class RegisterRequestDTO {
 
   private String firstname;
   private String lastname;
